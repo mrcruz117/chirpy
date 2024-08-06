b499556d51a6a39a6f28a1390c4b00d02867c468
mrcruz117 <mrcruz1989@gmail.com>
2024-08-05 21:44:52 -0400

    handling html get req

--- main.go
+++ main.go
@@ -9,6 +9,9 @@ func main() {
 	// create a new ServeMux
 	mux := http.NewServeMux()
 
+	fileServer := http.FileServer(http.Dir("."))
+
+	mux.Handle("/", fileServer)
 	// create a new server
 	server := &http.Server{
 		Addr:    ":8080",
