ff733fd272ae726b256550434f1c8fe1f84b1f86
Michael Cruz <mcruz@aersale.com>
2024-12-16 16:15:14 -0500

    rebuild start

--- main.go
+++ main.go
@@ -3,18 +3,16 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 
 	// "github.com/golang-jwt/jwt/v5"
 	"github.com/joho/godotenv"
-	"github.com/mrcruz117/chirpy/internal/database"
 )
 
 type apiConfig struct {
 	fileserverHits int
-	DB             *database.DB
-	jwtSecret      string
-	polkaKey       string
+	// DB             *database.DB
+	jwtSecret string
+	polkaKey  string
 }
 
 func main() {
@@ -24,47 +22,15 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-	jwtSecret := os.Getenv("JWT_SECRET")
-	polkaKey := os.Getenv("POLKA_KEY")
-	if jwtSecret == "" {
-		log.Fatal("JWT_SECRET environment variable is not set")
-	}
+	// jwtSecret := os.Getenv("JWT_SECRET")
+	// polkaKey := os.Getenv("POLKA_KEY")
+	// if jwtSecret == "" {
+	// 	log.Fatal("JWT_SECRET environment variable is not set")
+	// }
 	const filepathRoot = "."
 	const port = "8080"
 
-	db, err := database.NewDB("database.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	apiCfg := apiConfig{
-		fileserverHits: 0,
-		DB:             db,
-		jwtSecret:      jwtSecret,
-		polkaKey:       polkaKey,
-	}
-
 	mux := http.NewServeMux()
-
-	mux.Handle("/app/*", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
-
-	mux.HandleFunc("GET /api/healthz", handlerReadiness)
-	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
-	mux.HandleFunc("GET /api/reset", apiCfg.handlerReset)
-
-	mux.HandleFunc("POST /api/chirps", apiCfg.handlerChirpsCreate)
-	mux.HandleFunc("GET /api/chirps", apiCfg.handlerChirpsRetrieve)
-	mux.HandleFunc("GET /api/chirps/{chirpID}", apiCfg.handlerChirpByID)
-	mux.HandleFunc("DELETE /api/chirps/{chirpID}", apiCfg.handlerChirpsDelete)
-
-	mux.HandleFunc("POST /api/users", apiCfg.handlerUsersCreate)
-	mux.HandleFunc("PUT /api/users", apiCfg.handlerUsersUpdate)
-
-	mux.HandleFunc("POST /api/login", apiCfg.handlerLogin)
-	mux.HandleFunc("POST /api/refresh", apiCfg.handlerRefresh)
-	mux.HandleFunc("POST /api/revoke", apiCfg.handlerRevoke)
-
-	mux.HandleFunc("POST /api/polka/webhooks", apiCfg.handlerWebhook)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
