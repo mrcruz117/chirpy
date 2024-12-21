336537cdf13e5aee191038f3d8891766fabcf2f8
mrcruz117 <mrcruz1989@gmail.com>
2024-12-21 17:48:12 -0500

    completed pw impl and login check

--- main.go
+++ main.go
@@ -53,6 +53,7 @@ func main() {
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
 
 	mux.HandleFunc("POST /api/users", apiCfg.handlerUsersCreate)
+	mux.HandleFunc("POST /api/login", apiCfg.handlerLogin)
 
 	mux.HandleFunc("GET /api/chirps", apiCfg.handlerChirpsGet)
 	mux.HandleFunc("POST /api/chirps", apiCfg.handlerChirpsCreate)
