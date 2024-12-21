6ce3da6fe82228e1484c7e241a7b11613127de70
mrcruz117 <mrcruz1989@gmail.com>
2024-12-21 16:55:47 -0500

    get chirp by id

--- main.go
+++ main.go
@@ -56,7 +56,7 @@ func main() {
 
 	mux.HandleFunc("GET /api/chirps", apiCfg.handlerChirpsGet)
 	mux.HandleFunc("POST /api/chirps", apiCfg.handlerChirpsCreate)
-
+	mux.HandleFunc("GET /api/chirps/{chirpID}", apiCfg.handlerChirpsGetByID)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 
